6d168c042cbb1a361ad317cf97b42f4b89ec0a37
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2021-12-29 22:11:37 +0900

    Add files via upload

---
+++ 1/CPP_Tutorial.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CPP_Tutorial.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CPP_Tutorial, "CPP_Tutorial" );
+
+DEFINE_LOG_CATEGORY(MyLogCategory);
---
+++ 1/CPP_Tutorial.h
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+// 로그 카테고리 생성
+DECLARE_LOG_CATEGORY_EXTERN(MyLogCategory, Log, All);
