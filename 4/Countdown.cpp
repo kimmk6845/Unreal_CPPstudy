efc4643a8693500df0a8551fed86a54611fb9442
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2022-01-01 11:24:34 +0900

    Add files via upload

---
+++ 4/Countdown.cpp
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Countdown.h"
+
+// Sets default values
+ACountdown::ACountdown()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;	// Tick Disable
+
+	CountdownText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Countdown Text"));
+	CountdownText->SetHorizontalAlignment(EHTA_Center);		// 랜더링 텍스트 가운데 정렬
+	CountdownText->SetWorldSize(150.0f);
+	RootComponent = CountdownText;	// 루트 컴포넌트로 만들어줌
+	CountdownTime = 3;
+}
+
+// Called when the game starts or when spawned
+void ACountdown::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UpdateTimerDisplay(); // 생성자에서 초기화된 CountdownTime을 출력
+
+	GetWorldTimerManager().SetTimer(CountdownTimerHandle, this, &ACountdown::AdvanceTimer, 1.0f, true);	// 타이머 실행
+
+}
+
+void ACountdown::UpdateTimerDisplay()
+{
+	// 변수를 이용해 화면에 숫자를 표시
+	CountdownText->SetText(FString::FromInt(FMath::Max(CountdownTime, 0)));
+}
+
+void ACountdown::AdvanceTimer()
+{
+	--CountdownTime;
+	UpdateTimerDisplay(); // 텍스트 업데이트
+
+	if (CountdownTime < 1)	// 카운트다운이 0이 되면,
+	{
+		GetWorldTimerManager().ClearTimer(CountdownTimerHandle); // 타이머 멈춤
+		CountdownHasFinished();
+	}
+}
+
+void ACountdown::CountdownHasFinished_Implementation()
+{
+	CountdownText->SetText(TEXT("Go!"));
+}
---
+++ 4/Countdown.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Engine/Classes/Components/TextRenderComponent.h>	// 헤더를 include할 때 generated.h 위에 선언해야 함!!!!
+#include "Countdown.generated.h"
+
+UCLASS()
+class VARIABLE_TIMER_EVENT_API ACountdown : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACountdown();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+
+	// 화면에 숫자를 표시하는 작업
+
+	// 에디터에서 변수 값을 바꿀 수 있도록 함
+	UPROPERTY(EditAnywhere)
+	int32 CountdownTime;
+
+	UTextRenderComponent* CountdownText;
+
+	void UpdateTimerDisplay();
+
+	// 시간을 체크할 타이머 추가하는 작업
+	void AdvanceTimer();	// 타이머가 돌아가면서 호출될 함수
+
+	// 함수를 블루프린트에 공개해 기능을 수정할 수 있도록
+	UFUNCTION(BlueprintNativeEvent)
+	void CountdownHasFinished(); // AdvanceTimer함수가 타이머가 지정한 만큼 작동한 뒤에 마무리 처리하는 함수
+	virtual void CountdownHasFinished_Implementation();
+
+	FTimerHandle CountdownTimerHandle;	// FTimerHandle은 타이머를 컨트롤하기 위한 구조체로 카운트다운이 끝났을때 타이머가 지속해서 돌아가지 않도록 종료하는 변수
+
+
+};
