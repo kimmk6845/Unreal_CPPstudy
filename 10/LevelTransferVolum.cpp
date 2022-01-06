065a52ed2b98974d1fe20d8431ab944200c295ab
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2022-01-06 15:11:22 +0900

    Add files via upload

---
+++ 10/LevelTransferVolum.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LevelTransferVolum.h"
+#include "OpenLevelCharacter.h"
+#include <Engine/Classes/Components/BoxComponent.h>
+#include <Kismet/GameplayStatics.h>	// 레벨 이동
+// Sets default values
+ALevelTransferVolum::ALevelTransferVolum()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+
+	TransferVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TransferVolume"));
+	RootComponent = TransferVolume;
+	TransferVolume->SetCollisionProfileName(TEXT("OverlapOnlyPawn"));	// 폰만 감지하도록
+}
+
+// Called when the game starts or when spawned
+void ALevelTransferVolum::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALevelTransferVolum::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+void ALevelTransferVolum::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	AOpenLevelCharacter* Character = Cast<AOpenLevelCharacter>(OtherActor);	// 콜리전에 닿은 액터가 주인공 케릭터여야 함
+	if (Character)
+	{
+		// 레벨을 이동시킴
+		UGameplayStatics::OpenLevel(GetWorld(),FName(TransferLevelName));
+	}
+}
---
+++ 10/LevelTransferVolum.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LevelTransferVolum.generated.h"
+
+UCLASS()
+class OPENLEVEL_API ALevelTransferVolum : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALevelTransferVolum();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;	// 콜리전 감지
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	FString TransferLevelName;	// 이 액터에 닿았을 때 씬의 이름을 저장
+
+	UPROPERTY()
+	class UBoxComponent* TransferVolume;	// 콜리전 영역
+};
