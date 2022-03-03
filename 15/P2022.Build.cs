d1136e582ee98f92ce12049a0ab82639d7d355a5
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2022-03-03 22:33:44 +0900

    Add files via upload

--- 15/P2022.Build.cs
+++ 15/P2022.Build.cs
@@ -9,6 +9,7 @@ public class P2022 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "Niagara" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		PublicIncludePaths.Add(ModuleDirectory);
 	}
