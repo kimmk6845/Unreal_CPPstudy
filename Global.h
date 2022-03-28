a43b27964a6163edf011d89752064805e4684a01
kimmk6845 <71704350+kimmk6845@users.noreply.github.com>
2022-03-28 22:18:09 +0900

    Delete Global.h

--- Global.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "DrawDebugHelpers.h"	// DrawDebug
-
-#include "Kismet/GameplayStatics.h"
-#include "Kismet/KismetSystemLibrary.h"
-#include "Kismet/KismetMathLibrary.h"	
-
-#include "Utilities/CHelpers.h"
